db8a098e368e6f181d53a314213880a0b760a1c8
Asperity <strider1606@gmail.com>
2026-06-25 10:48:22 -0400

    added express server and ngrok

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import express from "express";
 const app = express();
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type' : 'text/html'});
+    const html = `
+    <html>
+    <head><title>Scattergories</title></head>
+    <body><h1>Endpoint game to make a session</h1></body>
+    </html>
+    `;
+    res.end(html);
+});
+
+const port = 8080;
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
