705f343242a9b9dc77824144c2fe3a5345acf73a
salioudunn11 <salioudunn11@gmail.comclean>
2026-06-25 14:02:07 -0400

    answers 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Devonte Allen
 Muhammad Usman
 Mamadou Dunn
+
+We asked Professor and TAs for help. Also use the github repo as a reference. Used AI to help creating certain rows.
--- srcs/answers.js
+++ srcs/answers.js
@@ -6,28 +6,45 @@ const router = express.Router();
 
 router.post("/", async (req, res, next) => {
     try{
-        const {roomCode, username, answer} = req.body;
+        const {room_codes, player_id, text} = req.body;
 
-        if (typeof roomCode !== "string" || roomcode.trim() === ""){
+        if (typeof room_codes !== "string" || room_codes.trim() === ""){
             return req.status (400).json({error: "roomCode is required"});
         }
-        if (typeof username !== "string" || answer.trim() === ""){
+        if (typeof player_id !== "string" || player_id.trim() === ""){
             return req.status (400).json({error: "username is required"});
             
         }
-        if (typeof answer !== "string" || answer.trim() === ""){
+        if (typeof text !== "string" || text.trim() === ""){
             return req.status (400).json({error: "answers is required"});
 
         }
-        const clean_room = roomCode.trim();
-        const clean_answer = answer.trim();
+        const clean_room = room_codes.trim();
+        const clean_answer = text.trim();
+
+
 
         const game = await prisma.game.findUnique({
-            where: {roomCode: clean_room}
+            where: {room_codes: clean_room}
         });
         if (!game) {
-            return res.status (404).json({error:})
+            return res.status (404).json({error: "No game with that room code"});   
         }
+        if (clean_answer[0].toUpperCase() !== game.letter.toUpperCase()) {
+            return res
+                .status (400)
+                .json({error: `Answer must start with ${game.letter}`});
+        }
+
+
+        await prisma.answer.create({
+            data: {
+                game_id: game.id,
+                player_id: player_id.trim(),
+                text: clean_answer,
+            },
+        });
+
 
     }
 })
