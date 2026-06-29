4345c4dcddd1136648867bb54bfcc94abca218d9
AbuTalha63 <usmanm6390@gmail.com>
2026-06-29 10:47:22 -0400

    typo fixes

--- srcs/answers.js
+++ srcs/answers.js
@@ -36,14 +36,14 @@ router.post("/", async (req, res, next) => {
         if (!game) {
             return res.status (404).json({error: "No game with that room code"});   
         }
-        if (clean_text[0].toUpperCase() !== game.letter.toUpperCase()) {
+        if (clean_text[0].toUpperCase() !== game.letters.toUpperCase()) {
             return res
                 .status (400)
-                .json({error: `Answer must start with ${game.letter}`});
+                .json({error: `Answer must start with ${game.letters}`});
         }
 
 
-        await prisma.text.create({
+        await prisma.answer.create({
             data: {
                 game_id: game.id,
                 username: username.trim(),
