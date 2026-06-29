5bc58ccfc9526e38b913e3ee27ecdf95c39808bb
salioudunn11 <salioudunn11@gmail.comclean>
2026-06-29 11:12:16 -0400

    added listen

--- srcs/index.js
+++ srcs/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import gamesRouter from "./games.js";
 import answersRouter from "./answers.js";
 
-// import dbRouter from "./db.js";
-
 const app = express();
 
 app.use(express.json());
@@ -11,13 +9,19 @@ app.use(express.json());
 app.use("/games", gamesRouter);
 app.use("/answers", answersRouter);
 
-app.use((req,res) => {
-    res.status(404).json({error: "Not found"});
-})
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({error: "Something went wrong"});
+  console.error(err);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
